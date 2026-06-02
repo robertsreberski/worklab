b573a1f432b75fb15caf989dd8b82056c09a9488
Robert Sreberski <robertsreberski@gmail.com>
2026-06-02 14:10:02 +0000

    Internalize icon glyph table

--- src/ui/src/components/Icon.jsx
+++ src/ui/src/components/Icon.jsx
@@ -1,4 +1,4 @@
-export const ICONS = {
+const ICONS = {
   "alert-triangle": '<path d="M10.29 3.86 1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"/><line x1="12" y1="9" x2="12" y2="13"/><line x1="12" y1="17" x2="12.01" y2="17"/>',
   "arrow-right": '<line x1="5" y1="12" x2="19" y2="12"/><polyline points="12 5 19 12 12 19"/>',
   book: '<path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>',
